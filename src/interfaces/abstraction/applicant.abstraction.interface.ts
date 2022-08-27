a5f054114c7b3d99f821a5de2fd34de7ab5d9bca
legendateam <undertaker176@gmail.com>
2022-08-27 23:01:09 +0300

    add method in abstraction interface

--- src/interfaces/abstraction/applicant.abstraction.interface.ts
+++ src/interfaces/abstraction/applicant.abstraction.interface.ts
@@ -2,11 +2,15 @@ import { IApplicantModel } from '../models/applicant.model.interface';
 import { ApplicantParamsId, ApplicationToAdd } from '../../types';
 import { IDeleteFromDb } from '../delete-from-db.interface';
 import { IApplicantToSet } from '../applicant-to-set.interface';
+import { IApplicantInteresting } from '../applicant-interesting.interface';
+import { CategoriesEnum } from '../../enums';
 
 export interface IApplicantAbstraction {
     getOneByEmail(email: string): Promise<IApplicantModel | null>,
     getOneById({ applicant_id }: ApplicantParamsId): Promise<IApplicantModel | null>,
     addOne(applicationToAdd: ApplicationToAdd): Promise<IApplicantModel>,
     updateFullFields({ _id, applicantToSet }: IApplicantToSet): Promise<IApplicantModel | null>,
-    deleteOne({ applicant_id }: ApplicantParamsId):Promise<IDeleteFromDb>
+    deleteOne({ applicant_id }: ApplicantParamsId):Promise<IDeleteFromDb>,
+    getAllByFilters(filters: IApplicantInteresting, category: CategoriesEnum)
+        : Promise<IApplicantModel[] | null>
 }
